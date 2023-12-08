fc7a5f7eff84fc5d7e42f8625ef113b53dd0be38
EnzoPlayer0ne <enzo@desiage.org>
2023-12-08 20:18:31 +0100

    save

--- rust/0217-contains-duplicate.rs
+++ rust/0217-contains-duplicate.rs
@@ -3,16 +3,13 @@ use std::collections::HashSet;
 impl Solution {
     pub fn contains_duplicate(nums: Vec<i32>) -> bool {
         let mut map = HashSet::new();
-        
-        for &n in nums.iter(){
 
-            if map.contains(&n){
-                return true; 
+        for n in nums {
+            if !map.insert(n){
+                return true;
             }
-            
-            map.insert(n);          
-        };        
-        
+        }
+
         false
     }
 }
